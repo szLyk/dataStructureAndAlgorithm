7b172627c3951ebf42f5b659562690e8479d0edd
szlyk <349978892@qq.com>
2022-06-06 00:34:11 +0800

    1.0

---
+++ dataStructure/src/main/java/sparsearray/SparseArray.java
@@ -0,0 +1,25 @@
+package sparsearray;
+
+public class SparseArray {
+
+    public static void main(String[] args) {
+        // 创建一个原始的二维数组 11 * 11
+        // 0: 表示没有棋子， 1 表示 黑子 2 表蓝子
+        int chessArr1[][] = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+        chessArr1[4][5] = 1;
+        // 输出原始的二维数组
+        System.out.println("原始的二维数组~~");
+        for (int[] row : chessArr1) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        int length = chessArr1.length;
+        System.out.println(length);
+
+    }
+}
