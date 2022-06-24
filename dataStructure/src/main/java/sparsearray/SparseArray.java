8dd69d17a7ae190409e4e2a46ac9908e8382dbbe
yikai.luo <yikai.luo@vantopgroup.com>
2022-06-24 17:26:09 +0800

    稀疏数组 1.0

--- dataStructure/src/main/java/sparsearray/SparseArray.java
+++ dataStructure/src/main/java/sparsearray/SparseArray.java
@@ -9,6 +9,7 @@ public class SparseArray {
         chessArr1[1][2] = 1;
         chessArr1[2][3] = 2;
         chessArr1[4][5] = 1;
+        chessArr1[7][6] = 2;
         // 输出原始的二维数组
         System.out.println("原始的二维数组~~");
         for (int[] row : chessArr1) {
@@ -28,29 +29,63 @@ public class SparseArray {
             for (int j = 0; j < length; j++) {
                 int i1 = chessArr1[i][j];
 
-                if (i1 != 0){
-                    count ++;
+                if (i1 != 0) {
+                    count++;
                 }
             }
         }
 
-
-
-        int[][] ints = new int[count][2];
+        //建立稀疏数组，存数据情况
+        int[][] ints = new int[count + 1][3];
         ints[0][0] = high;
         ints[0][1] = length;
         ints[0][2] = count;
 
+        int a = 0;
 
-        for (int i = 0; i < high; i++) {
 
-            for (int j = 0; j < length; j++) {
-                int i1 = chessArr1[i][j];
+        for (int i = 0; i < chessArr1.length; i++) {
+            for (int j = 0; j < chessArr1[i].length; j++) {
 
-                if (i1 != 0){
-
+                if (chessArr1[i][j] != 0) {
+                    a++;
+                    ints[a][0] =  i;
+                    ints[a][1] =  j;
+                    ints[a][2] = chessArr1[i][j];
                 }
             }
+        }
+        //展示
+        System.out.println();
+        System.out.println("稀疏数组");
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints[i].length; j++) {
+                System.out.print(ints[i][j] +"\t");
+            }
+            System.out.println();
+        }
+        System.out.println("*******************");
+
+        //解析稀疏数组
+
+        int newArrayHigh = ints[0][0];
+        int newArrayLength = ints[0][1];
+        int nu= ints[0][2] ;
+
+        int[][] newArray = new int[newArrayHigh][newArrayLength];
+
+        for (int i = 1; i < ints.length; i++) {
+
+           int nuHigh = ints[i][0];
+           int nuLength = ints[i][1];
+           newArray[nuHigh][nuLength] = ints[i][2];
+        }
+        System.out.println("还原数组~");
+        for (int[] ints1 : newArray) {
+            for (int i : ints1) {
+                System.out.print(i+"\t");
+            }
+            System.out.println();
         }
 
 
