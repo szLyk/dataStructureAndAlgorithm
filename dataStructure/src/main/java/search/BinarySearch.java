660167d7f233a3a49563b64ef77eafcd22b6cff9
yikai.luo <yikai.luo@vantopgroup.com>
2022-07-11 17:21:38 +0800

    二分查找 1.0

---
+++ dataStructure/src/main/java/search/BinarySearch.java
@@ -0,0 +1,38 @@
+package search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        //int arr[] = { 1, 8, 10, 89,1000,1000, 1234 };
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    }
+
+    // 二分查找算法
+
+    /**
+     * @param arr     数组
+     * @param left    左边的索引
+     * @param right   右边的索引
+     * @param findVal 要查找的值
+     * @return 如果找到就返回下标，如果没有找到，就返回 -1
+     */
+    public static int binarySearch(int[] arr, int left, int right, int findVal) {
+
+        // 当 left > right 时，说明递归整个数组，但是没有找到
+        if (left > right) {
+            return -1;
+        }
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+
+        if (findVal > midVal) { // 向 右递归
+            return binarySearch(arr, mid + 1, right, findVal);
+        } else if (findVal < midVal) { // 向左递归
+            return binarySearch(arr, left, mid - 1, findVal);
+        } else {
+
+            return mid;
+        }
+    }
+
+}
